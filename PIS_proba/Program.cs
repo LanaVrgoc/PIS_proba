79106373717eae3318a27bacdfe26ad1002d5340
LanaVrgoc <lanavrgoc56@gmail.com>
2026-04-01 20:43:05 +0200

    test

--- PIS_proba/Program.cs
+++ PIS_proba/Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+namespace PIS_proba
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
